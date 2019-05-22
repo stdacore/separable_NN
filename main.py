3fe76d58c21c6cba9d1f72499010a816c913f1ac
stdacore <r06921060@ntu.edu.tw>
2019-05-22 23:18:56 +0800

    update report

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ print('save: %s'%args.save)
 print('===== parameter settings =====')
 
 
-device = 'cuda:%d'%args.cuda 
+device = 'cuda'#'cuda:%d'%args.cuda 
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
@@ -74,9 +74,18 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch, shuffle
 # Model
 print('==> Building model..')
 # net = sresnet164_cifar(num_classes=100)
-net = resneXt_cifar(56, 4, 16, num_classes=100)
-# net = densenet_BC_cifar(100, 12, num_classes=100)
+net = resneXt_cifar(110, 4, 16, num_classes=100, is_separate=True)
+# net = densenet_BC_cifar(250, 24, num_classes=100)
+net = nn.DataParallel(net)
 net = net.to(device)
+# net2 = resneXt_cifar(56, 1, 16, num_classes=100, is_separate=True)
+# net2 = net2.to(device)
+# net3 = resneXt_cifar(56, 1, 16, num_classes=100, is_separate=True)
+# net3 = net3.to(device)
+# net4 = resneXt_cifar(56, 1, 16, num_classes=100, is_separate=True)
+# net4 = net4.to(device)
+# net5 = resneXt_cifar(56, 1, 16, num_classes=100, is_separate=True)
+# net5 = net4.to(device)
 
 # for p in net.parameters():
 #     print(p.nelement())
@@ -87,17 +96,23 @@ if args.resume:
 
     print('==> Resuming from checkpoint..')
     assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-    # checkpoint = torch.load('./checkpoint/resnet164_2_1c100_192.t7')
-    checkpoint = torch.load('./checkpoint/%s'%args.resume)
-    for key in checkpoint['net'].keys():
-        if 'layer' in key and 'num_batches_tracked' not in key:
-            x = checkpoint['net'][key]
-            shape = x.shape
+#     checkpoint = torch.load('./checkpoint/%s'%args.resume)
+    checkpoint = torch.load('./checkpoint/resnext_56_1_16_167.t7')
+    checkpoint2 = torch.load('./checkpoint/resnext_56_1_16_re2_198.t7')
+    checkpoint5 = torch.load('./checkpoint/resnext_56_1_16_re5_157.t7')
+    checkpoint3 = torch.load('./checkpoint/resnext_56_1_16_re3_155.t7')
+    checkpoint4 = torch.load('./checkpoint/resnext_56_1_16_re4_159.t7')
+    
+#     for key in checkpoint['net'].keys():
+#         if 'layer' in key and 'num_batches_tracked' not in key:
+#             checkpoint['net'][key] = torch.cat([checkpoint['net'][key], checkpoint2['net'][key], checkpoint3['net'][key], checkpoint4['net'][key]], 0)
+#             x = checkpoint['net'][key]
+#             shape = x.shape
 #             shape[0] = shape[0]//4
 #             r = random.randint(0, shape[0]*3)
-            checkpoint['net'][key] = torch.cat([x, x, x, x], 0)
-            tensor = torch.randn(checkpoint['net'][key].shape)/50
-            checkpoint['net'][key] = checkpoint['net'][key].to('cpu')+tensor
+#             checkpoint['net'][key] = torch.cat([x, x, x, x], 0)
+#             tensor = torch.randn(checkpoint['net'][key].shape)/50
+#             checkpoint['net'][key] = checkpoint['net'][key].to('cpu')+tensor
 #         substring = key.split('device_')
 #         from_key = substring[0]+'device_1'+substring[1][1:] if len(substring)>=2 else key
 #         if key!=from_key:
@@ -105,6 +120,10 @@ if args.resume:
 
 
     net.load_state_dict(checkpoint['net'])
+    net2.load_state_dict(checkpoint2['net'])
+    net3.load_state_dict(checkpoint3['net'])
+    net4.load_state_dict(checkpoint4['net'])
+    net5.load_state_dict(checkpoint5['net'])
     best_acc = checkpoint['acc']
     start_epoch = checkpoint['epoch']
 
@@ -142,6 +161,10 @@ def train(epoch):
 def test(epoch):
     global best_acc
     net.eval()
+#     net2.eval()
+#     net3.eval()
+#     net4.eval()
+#     net5.eval()
     test_loss = 0
     correct = 0
     total = 0
@@ -149,6 +172,7 @@ def test(epoch):
         for batch_idx, (inputs, targets) in enumerate(testloader):
             inputs, targets = inputs.to(device), targets.to(device)
             outputs = net(inputs)
+#             outputs = (net(inputs)+net2(inputs)+net3(inputs)+net5(inputs))
             loss = criterion(outputs, targets)
 
             test_loss += loss.item()
@@ -171,7 +195,9 @@ def test(epoch):
             }
             if not os.path.isdir('checkpoint'):
                 os.mkdir('checkpoint')
+            net.to('cpu')
             torch.save(state, './checkpoint/%s_%d.t7'%(args.save, epoch))
+            net.to(device)
         best_acc = acc
         
 for epoch in range(args.epoch):
@@ -186,3 +212,4 @@ for epoch in range(args.epoch):
     print('now learning rate is %.4f'%optimizer.param_groups[0]['lr'])
     
 
+#python3 main.py --lr 0.1 --split 1 --epoch 200 --batch 128 --schedule 50 --cuda 1 --save resnext_56_2_16
--- snn_graph_2.py
+++ snn_graph_2.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import math
 
 # class GroupBN(nn.Module):
@@ -38,22 +39,36 @@ class SELayer(nn.Module):
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, inplanes, planes, cardinality, baseWidth, stride=1, downsample=None):
+    def __init__(self, inplanes, planes, cardinality, baseWidth, stride=1, downsample=None, separate_coef=1):
         super(Bottleneck, self).__init__()
-        D = int(planes * (baseWidth / (16*cardinality)))
-        C = cardinality
-        self.conv1 = nn.Conv2d(inplanes, D*C, kernel_size=1, groups=C, bias=False)
+        D = int(planes * (baseWidth / (16*separate_coef)))##
+        C = cardinality*2
+        self.conv1 = nn.Conv2d(inplanes, D*C, kernel_size=1, groups=separate_coef, bias=False)
         self.bn1 = nn.BatchNorm2d(D*C)
         self.conv2 = nn.Conv2d(D*C, D*C, kernel_size=3, stride=stride, padding=1, groups=C, bias=False)
         self.bn2 = nn.BatchNorm2d(D*C)
-        self.conv3 = nn.Conv2d(D*C, planes*4, kernel_size=1, groups=C, bias=False)
+        self.conv3 = nn.Conv2d(D*C, planes*4, kernel_size=1, groups=separate_coef, bias=False)
         self.bn3 = nn.BatchNorm2d(planes*4)
-#         self.se = SELayer(planes*4, groups=C, reduction=4)
+        self.se = SELayer(planes*4, groups=separate_coef, reduction=4)
+#         self.extract = nn.Sequential(
+#             nn.Conv2d(inplanes, inplanes*4, kernel_size=3, stride=1, padding=1, groups=separate_coef, bias=False),
+#             nn.BatchNorm2d(inplanes*4),
+#             nn.ReLU(inplace=True),
+#             nn.PixelShuffle(2)
+#         )
+#         self.extract = nn.Sequential(
+#             nn.ConvTranspose2d(inplanes, inplanes, kernel_size=2, stride=2, padding=0, groups=separate_coef, bias=False),
+#             nn.BatchNorm2d(inplanes),
+#             nn.ReLU(inplace=True),
+#         )
+#         self.compress = nn.AvgPool2d(2)
+
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
 
-    def forward(self, x):
+    def forward(self, x, y=None):
+        
         residual = x
 
         out = self.conv1(x)
@@ -63,10 +78,26 @@ class Bottleneck(nn.Module):
         out = self.conv2(out)
         out = self.bn2(out)
         out = self.relu(out)
+        
+#         shift = out.shape[1]//4*(y%(4-1)+1)
+#         y = torch.cat([out[:,shift:], out[:,:shift]], 1)
+#         out = (out+y)/2
+#         shift = out.shape[1]//4*(y%(4-1)+1)
+#         y = torch.cat([out[:,shift:], out[:,:shift]], 1)
+#         out = (out+y)/2
+        
+#         if y is None:
+#             y = out
+#         else:
+#             if y.shape!=out.shape:
+#                 y = F.avg_pool2d(y, 2, 2)
+#             out = (out+y)/2
+#             y = None
+
 
         out = self.conv3(out)
         out = self.bn3(out)
-#         out = self.se(out)
+        out = self.se(out)
 
         if self.downsample is not None:
             residual = self.downsample(x)
@@ -75,25 +106,26 @@ class Bottleneck(nn.Module):
             print(out.size(), residual.size())
         out += residual
         out = self.relu(out)
-
+        
         return out
 
 
 class ResNeXt_Cifar(nn.Module):
 
-    def __init__(self, block, layers, cardinality, baseWidth, num_classes=10):
+    def __init__(self, block, layers, cardinality, baseWidth, num_classes=10, is_separate=True):
         super(ResNeXt_Cifar, self).__init__()
-        self.inplanes = 16*cardinality #64
         self.cardinality = cardinality
+        self.separate_coef = cardinality if is_separate else 1
+        self.inplanes = 16*self.separate_coef #64##
         self.baseWidth = baseWidth
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(16)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)##
+        self.bn1 = nn.BatchNorm2d(16)##
         self.relu = nn.ReLU(inplace=True)
-        self.layer1 = self._make_layer(block, 16*cardinality, layers[0])
-        self.layer2 = self._make_layer(block, 16*cardinality*2, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 16*cardinality*4, layers[2], stride=2)
+        self.layer1 = self._make_layer(block, 16*self.separate_coef, layers[0])##
+        self.layer2 = self._make_layer(block, 16*self.separate_coef*2, layers[1], stride=2)##
+        self.layer3 = self._make_layer(block, 16*self.separate_coef*4, layers[2], stride=2)##
         self.avgpool = nn.AvgPool2d(8, stride=1)
-        self.fc = nn.Linear(64 * block.expansion, num_classes)
+        self.fc = nn.Linear(64 * block.expansion, num_classes)##
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -107,15 +139,15 @@ class ResNeXt_Cifar(nn.Module):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1, stride=stride, groups=self.cardinality, bias=False),
+                nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1, stride=stride, groups=self.separate_coef, bias=False),
                 nn.BatchNorm2d(planes * block.expansion)
             )
 
         layers = []
-        layers.append(block(self.inplanes, planes, self.cardinality, self.baseWidth, stride, downsample))
+        layers.append(block(self.inplanes, planes, self.cardinality, self.baseWidth, stride, downsample, separate_coef=self.separate_coef))
         self.inplanes = planes * block.expansion
         for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes, self.cardinality, self.baseWidth))
+            layers.append(block(self.inplanes, planes, self.cardinality, self.baseWidth, separate_coef=self.separate_coef))
         
         return nn.Sequential(*layers)
 
@@ -123,28 +155,41 @@ class ResNeXt_Cifar(nn.Module):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
-        x = torch.cat([x for _ in range(self.cardinality)], 1)
-        device_num = self.cardinality
-        s = 0
-        for stage in [self.layer1, self.layer2, self.layer3]:
-            for i, layer in enumerate(list(stage.modules())[0]):
-                x = layer(x)
-                if i%2==0:
-                    shift = x.shape[1]//device_num*(s%(device_num-1)+1)
-                    a = torch.cat([x[:,shift:], x[:,:shift]], 1)
-#                     a = a.half()
-#                     a = a.float()
-                    s += 1
-                elif i%2==1:
-                    x = (x+a)/2
-
-        start = 0*x.shape[1]//device_num
-        end = 1*x.shape[1]//device_num
-        x = x[:,start:end]
         
-#         x = self.layer1(x)
-#         x = self.layer2(x)
-#         x = self.layer3(x)
+        if self.separate_coef>1:
+            x = torch.cat([x for _ in range(self.cardinality)], 1)
+            device_num = self.cardinality
+            s = 0
+            for stage in [self.layer1, self.layer2, self.layer3]:
+                y = None
+                for i, layer in enumerate(list(stage.modules())[0]):
+#                     x, y = layer(x, s)
+#                     s += 1
+#                     if y is not None:
+#                         shift = y.shape[1]//device_num*(s%(device_num-1)+1)
+#                         y = torch.cat([y[:,shift:], y[:,:shift]], 1)
+#                         s += 1
+                    
+                    x = layer(x)
+                    if i%2==0:
+                        shift = x.shape[1]//device_num*(s%(device_num-1)+1)
+                        a = torch.cat([x[:,shift:], x[:,:shift]], 1)
+    #                     a = F.avg_pool2d(a, 2, 2)
+    #                     a = F.interpolate(a, x.shape[2:])
+    #                     a = a.half()
+    #                     a = a.float()
+                        s += 1
+                    elif i%2==1:
+                        x = (x+a)/2
+
+            start = 0*x.shape[1]//device_num
+            end = 1*x.shape[1]//device_num
+            x = x[:,start:end]
+        
+        else:
+            x = self.layer1(x)
+            x = self.layer2(x)
+            x = self.layer3(x)
 
 
         x = self.avgpool(x)
